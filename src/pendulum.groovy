e4dfc45c73c53d1b153d3c9c53cfe914a7cb753f
Arrvi <s11531@pjwstk.edu.pl>
2016-11-04 19:10:26 +0100

    Pendulum fix

--- src/pendulum.groovy
+++ src/pendulum.groovy
@@ -1,37 +1,43 @@
 
 
 import pl.edu.pja.s11531.psm.taylor.*
-def sin = new SinTaylorSeries().getClosure(25);
-def cos = new CosTaylorSeries().getClosure(25);
+// def sin = new SinTaylorSeries().getClosure(25);
+// def cos = new CosTaylorSeries().getClosure(25);
+def sin = Math.&sin
+
 
 def radius = 10.0
 def gravity = -10.0
-def timeStep = 0.1
+def timeStep = 0.05
 def angle = -1.0
 def angularVelocity = 0.0
-def steps = 300;
+def steps = 1000;
 def time = 0.0;
 
 
-def round = { it.setScale(10, java.math.RoundingMode.HALF_UP)}
+def round = { BigDecimal x -> x.setScale(10, java.math.RoundingMode.HALF_UP)}
 
 def ln = System.getProperty('line.separator')
 File csv = new File("../pendulum.csv")
 if (csv.exists()) csv.delete()
 csv << "time;angle;angular velocity;angular acceleration$ln"
 
-for (def i in (1..steps)) {
-	def angularAcceleration = gravity / radius * sin(angle)
-	def angVelHalfStep = angularVelocity + angularAcceleration * timeStep / 2
-	def angleStep = angle + angVelHalfStep * timeStep
-	def angleHalfStep = angle + angularVelocity * timeStep / 2
-	def angAccHalfStep = gravity / radius * sin(angleHalfStep)
-	def angVelStep = angularVelocity + angAccHalfStep * timeStep / 2
+def direction = 1
+def swings = 0
 
-	angle = angleStep
-	angularVelocity = angVelStep
+while (swings < 5) {
+	def angularAcceleration = gravity / radius * sin(angle)
+	def angVelHalfStep = angularVelocity + angularAcceleration * timeStep / 2.0
+	def angleHalfStep = angle + angularVelocity * timeStep / 2.0
+	def angAccHalfStep = gravity / radius * sin(angleHalfStep)
+	angle = angle + angVelHalfStep * timeStep
+	angularVelocity = angularVelocity + angAccHalfStep * timeStep
+
 	time += timeStep
 
-	println "Step $i (time=${round(time)}) angle=${round(angle)} aVelocity=${round(angularVelocity)} aAcceleration=${round(angularAcceleration)}"
+	if ( direction < 0 && angVelHalfStep > 0 ) swings++
+	direction = angVelHalfStep
+
+	println "time=${round(time)} angle=${round(angle)} aVelocity=${round(angularVelocity)} aAcceleration=${round(angularAcceleration)} swings=$swings"
 	csv << "${round(time)};${round(angle)};${round(angularVelocity)};${round(angularAcceleration)}$ln"
 }

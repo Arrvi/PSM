3fb2397d672a510af0663cd360123a3c9c66d6b5
Arrvi <s11531@pjwstk.edu.pl>
2016-11-15 15:42:24 +0100

    Added MaterialPoint and Projectile

---
+++ src/pl/edu/pja/s11531/psm/MaterialPoint.groovy
@@ -0,0 +1,10 @@
+package pl.edu.pja.s11531.psm
+
+/**
+ * Created by s11531 on 2016-11-15.
+ */
+class MaterialPoint {
+    BigDecimal mass
+    Vector position
+    Vector velocity
+}
---
+++ src/pl/edu/pja/s11531/psm/projectile/Projectile.groovy
@@ -0,0 +1,11 @@
+package pl.edu.pja.s11531.psm.projectile
+
+import pl.edu.pja.s11531.psm.MaterialPoint
+import pl.edu.pja.s11531.psm.Vector
+
+/**
+ * Created by s11531 on 2016-11-15.
+ */
+class Projectile extends MaterialPoint {
+    Vector acceleration
+}
